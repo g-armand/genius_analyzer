fbbea965541f47d22d5b6ae861a77864e127bdcb
g-armand <78274164+g-armand@users.noreply.github.com>
2022-02-02 18:28:03 +0100

    Update main.py
    updated clean_and_format() function

--- main.py
+++ main.py
@@ -16,35 +16,22 @@ import os							#working with files
 #name = str(artist's name)
 #title = str(title of the song)
 #index = int(just a counter)
-def write_file(link, name, title, index):
+def write_file(name, title, clean_text):
 	"""
-	When called, this function manages the html text retrieval, text cleaning and text storage for a song.
+	When called, this function manages the text storage for a song.
 	Does not return anything.
 	"""
-	
-	#call the get_right_version() function to get a clean text from genius.com
-	html_text = get_right_version(link)
-
-	#in case the clean text couldn't get retrieved from genius,
-	#we return None, and we let it know to the user
-	if html_text == "None":
-		print(f"{title}, n'a rien donné.")
-		return None
-
-	#cleaning of the html text
-	clean_text = clean_and_format(html_text)
 
 	#make a new directory to store songs
 	if os.path.isdir(name) == False:
 		os.mkdir(name)
 
 	#make a new file for each song
-	path = f"C:/Users/HP/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Python 3.10/prog/genius analyzer/{name}"
+	path = os.getcwd() + f"/{name}"
+	path = path.replace("\\", "/")
 	file_name_path = f"{path}/{name}_{title}.txt"
 	with open(file_name_path, 'w', encoding = 'utf-8') as f:
 		f.write(clean_text)
-
-	print(index) #can get erased
 
 #link: str(the genius.com link to the song)
 def get_right_version(link):
@@ -69,15 +56,15 @@ def get_right_version(link):
 		#when the string in key_word is in the html text, the final text is not well cleaned
 		#because beautiful soup does not handle well json files
 		key_word = "window.__PRELOADED_STATE__ = JSON.parse"
-		bad_version = key_word in html_text
 
+		bad_version = key_word not in html_text
 		#management of wrong format cases
 		if bad_version:
 			count += 1
 		if "Lyrics for this song have yet to be released." in html_text:
 			return "None"
 	if count >=10:
-		return "None"
+		return None
 
 	return html_text
 
@@ -91,28 +78,47 @@ def clean_and_format(text):
 		return "None"
 	#let's just cut the not useful parts of this text...
 	#the start...
+	i = 0
+	while i < len(text):
+		if text[i:i+39] in """window.__PRELOADED_STATE__ = JSON.parse""":
+			text = text[i:]
+			break
+		i += 1
 
 	i = 0
 	while i < len(text):
-		if text[i:i+34] in """<div initial-content-for="lyrics">""":
+		if text[i:i+33] in """lyricsData\\":{\\"body\\":{\\"html\\":\\"<p>[""":
 			text = text[i:]
 			break
 		i += 1
 
 	#...and the end
 	i = 0
-	while i < len(text):
-		if  text[i:i+48] == """<div initial-content-for="recirculated_content">""":
+	while i < len(text):        
+		if  text[i:i+30] == """,\\"children\\":[{\\"children\\":[""":
 			text = text[:i]
 			break
 		i+=1
-	
+
 	#BeautifulSoup is wonderful
 	soup = Bs(text, features="html.parser")
+
 	soup.p.encode('utf-8')
 	lyrics = soup.get_text()
+	lyrics = lyrics.replace("<\\/a>", "")
+	lyrics = lyrics.replace("<\\/i>", "")
+	lyrics = lyrics.replace("<\\/p>", "")
+	lyrics = lyrics.replace(r"\\n", "\n")
+	lyrics = lyrics.replace("\\", "")
+
+	#removing first line
+	lyrics = lyrics.strip()
+	lyrics = lyrics.split("\n")
+	lyrics = lyrics[1:]
+	lyrics = "\n".join(lyrics)
 
 	return lyrics.strip()
+
 
 #text:(str(clean text))
 def tokenize(text):
@@ -122,7 +128,7 @@ def tokenize(text):
 
 	#working on lines
 	liste_lines = text.split("\n")
-
+	print(liste_lines[0])
 	#working on words
 	for line in liste_lines:
 		liste_tokens.extend(line.split(" "))
@@ -189,7 +195,6 @@ def get_artist_songs(artist_id):
 	#going to next page
 	querystring['page'] = json_response['response']['next_page']
 
-	print(json_response)
 	#[next_page] contains an integer if there is next page of songs, None otherwise
 	while json_response['response']['next_page'] != None:
 
@@ -209,61 +214,70 @@ def get_artist_songs(artist_id):
 
 #songs: list(all songs obtained with get_artist_songs())
 #artist_name: str()
-def get_clean_titles(songs, artist_name):
+def get_clean_titles_and_text(title, artist_name):
 	"""
 	when called, will turn song title into its url format, for each song in songs.
 	it is then combined with artist_name to create the final genius url.
-	the url is then requested and if response == 200: the song's url is added to liste_url
-	returns a list of all working songs, and a list of their corresponding urls
+	the url is then requested and if response == 200: the song's url is used to retrieve a clean text with get_clean_text()
+	does not return anything
 	"""
 
 	#variable initialization
-	working_songs = []
-	liste_url = []
+
 	artist_name = artist_name.replace(' ', '-')
 
 	#replacing every character with their corresponding character
-	for index, title in enumerate(songs):
 
-		title = title.replace('é', 'e')
-		title = title.replace('è', 'e')
-		title = title.replace('ê', 'e')
-		title = title.replace('ë', 'e')
-		title = title.replace('à', 'a')
-		title = title.replace('â', 'a')
-		title = title.replace('ï', 'i')
-		title = title.replace('î', 'i')
-		title = title.replace('ù', 'u')
-		title = title.replace('û', 'u')
-		title = title.replace('ô', 'o')
-		title = title.replace('À', 'a')
-		title = title.replace('ç', 'c')
-		title = title.replace('Ç', 'c')
-		title = title.replace('ñ', 'n')
-		title = title.replace('/', "")
 
-		for i, elt in enumerate(title):
-			if elt not in " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-1234567890":
-				title = title.replace(elt, "")
+	title = title.replace('é', 'e')
+	title = title.replace('è', 'e')
+	title = title.replace('ê', 'e')
+	title = title.replace('ë', 'e')
+	title = title.replace('à', 'a')
+	title = title.replace('â', 'a')
+	title = title.replace('ï', 'i')
+	title = title.replace('î', 'i')
+	title = title.replace('ù', 'u')
+	title = title.replace('û', 'u')
+	title = title.replace('ô', 'o')
+	title = title.replace('À', 'a')
+	title = title.replace('ç', 'c')
+	title = title.replace('Ç', 'c')
+	title = title.replace('ñ', 'n')
+	title = title.replace('/', "")
+
+	for i, elt in enumerate(title):
+		if elt not in " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-1234567890":
+			title = title.replace(elt, "")
 		
-		#some titles have a weird format...		
-		title = title.replace('   ', '-')
-		title = title.replace('  ', '-')
-		title = title.replace(' ', '-')
+	#some titles have a weird format...		
+	title = title.replace('   ', '-')
+	title = title.replace('  ', '-')
+	title = title.replace(' ', '-')
 
-		if len(title) >= 1 and title[-1] == '-':
-			title = title[:-1]
+	if len(title) >= 1 and title[-1] == '-':
+		title = title[:-1]
 
-		url = f"https://genius.com/{artist_name}-{title}-lyrics"
+	url = f"https://genius.com/{artist_name}-{title}-lyrics"
 
-		#making request, append title and url if working
-		request = r.get(url)
-		if request.status_code == 200:
-			print(title)
-			working_songs.append(title)
-			liste_url.append(url)
-			
-	return working_songs, liste_url
+	#making request, append title and url if working
+	request = r.get(url)
+
+	if request.status_code != 200:
+		return None, None
+
+	elif request.status_code == 200:
+		#call the get_right_version() function to get a clean text from genius.com
+		text = get_right_version(url)
+		if text == "None":
+			return None, None
+		elif text:
+			#cleaning of the html text
+			clean_text = clean_and_format(text)
+			print('cleaning done')
+			return clean_text, title
+		else:
+			return None, None
 
 #search_term: str(artist's name)
 def main(search_term):
@@ -279,20 +293,21 @@ def main(search_term):
 		print("Unable to find artist's ID...")
 		return(None)
 
-	print(f"artist's ID ({artist_id})retrieved succesfully !")
+	print(f"artist's ID ({artist_id}) retrieved succesfully !")
 
 	#find all songs
 	all_songs = get_artist_songs(artist_id)
 	print(f"{len(all_songs)} songs retrieved")
 
-	#find all working songs
-	working_songs, liste_url = get_clean_titles(all_songs, search_term)
+	#work on each song
+	for song in all_songs:
+		#retrieve text
+		clean_text, title = get_clean_titles_and_text(song, search_term)
+		if clean_text == None:
+			pass
+		else:
+			#write a song.txt file for each working song 
+			search_term = search_term.replace(" ", "-")
+			write_file(search_term, title, clean_text)
+			print(title, 'writing done')
 
-	#write a song.txt file for each working song 
-	for index, url in enumerate(liste_url):
-		write_file(url, search_term, working_songs[index], index)
-
-
-#main('sofiane')
-
-a,b,c = int(), int(),int()
